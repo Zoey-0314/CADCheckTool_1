173d001094d9cd917186c1e908651d36409a24a3
hp <hp@DESKTOP-11SOJR4>
2026-08-11 11:34:25 +0800

    perf: optimize batch inspection and remove test commands

--- Batch/DrawingWeightCalculator.cs
+++ Batch/DrawingWeightCalculator.cs
@@ -1,5 +1,4 @@
-﻿using Autodesk.AutoCAD.DatabaseServices;
-using System;
+﻿using System;
 using System.IO;
 
 
@@ -13,110 +12,21 @@ namespace Correct_test1.Batch
             string file)
         {
 
-            double weight = 0;
-
             try
             {
+                // 旧方式为了计算进度打开并解析DWG，会造成一次额外解析。
+                // 新方式使用文件大小估算权重，避免重复加载DWG。
+                FileInfo info = new FileInfo(file);
+                double weight = info.Length / 1024.0 / 1024.0;
 
-                FileInfo info =
-                    new FileInfo(file);
-
-                // 文件大小权重
-                weight += info.Length / 1024.0 / 1024.0;
-
-                Database db =
-                    new Database(
-                        false,
-                        true
-                    );
-
-                db.ReadDwgFile(
-                    file,
-                    FileOpenMode.OpenForReadAndAllShare,
-                    false,
-                    ""
-                );
-
-                using (Transaction tr =
-                    db.TransactionManager.StartTransaction())
-                {
-
-                    //布局数量
-
-                    DBDictionary layouts =
-                        tr.GetObject(
-                            db.LayoutDictionaryId,
-                            OpenMode.ForRead
-                        )
-                        as DBDictionary;
-
-                    int layoutCount =
-                        layouts.Count;
-
-                    weight +=
-                        layoutCount * 5;
-
-                    //实体数量
-
-                    BlockTable bt =
-                        tr.GetObject(
-                            db.BlockTableId,
-                            OpenMode.ForRead
-                        )
-                        as BlockTable;
-
-                    foreach (ObjectId id in bt)
-                    {
-
-                        BlockTableRecord btr =
-                            tr.GetObject(
-                                id,
-                                OpenMode.ForRead
-                            )
-                            as BlockTableRecord;
-
-                        if (btr != null)
-                        {
-
-                            int entityCount = 0;
-
-                            foreach (ObjectId entId in btr)
-                            {
-                                entityCount++;
-                            }
-
-                            weight +=
-                                entityCount * 0.01;
-
-                        }
-
-                    }
-
-                    tr.Commit();
-
-                }
-
-                db.Dispose();
-
+                // 防止空文件或极小文件产生零权重。
+                return weight > 0 ? weight : 1;
             }
-            catch
+            catch (Exception)
             {
-
-                //读取失败给一个基础权重
-
-                weight = 1;
-
+                // 文件信息读取失败时使用基础权重，不影响正式检查流程。
+                return 1;
             }
-
-            //防止权重为0
-
-            if (weight <= 0)
-                weight = 1;
-
-            return weight;
-
         }
-
     }
-
 }
